265057e28a12dd11aa6a78a45b7a0b19aece7f29
Alain ZANKLAN <alainzanklan@gmail.com>
2024-12-02 03:26:13 +0000

    components fix

--- app/professionals/[id]/page.jsx
+++ app/professionals/[id]/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Procard from '@/components/ProCard';
+import ProCard from '@/components/ProCard';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
